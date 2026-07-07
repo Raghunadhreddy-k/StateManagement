01d8095103a1188d880b6549cce5e080f7e61cc9
raghu <raghu@DELL>
2026-07-07 12:03:36 +0530

    ApplicationState

---
+++ StateManagement/ApplicationState.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ApplicationState.aspx.cs" Inherits="StateManagement.ApplicationState" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblCount" runat="server" Text="Count"></asp:Label>
+            <asp:Button ID="btnIncrement" runat="server" Text="Increment" OnClick="btnIncrement_Click" />   
+        </div>
+    </form>
+</body>
+</html>
---
+++ StateManagement/ApplicationState.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateManagement
+{
+    public partial class ApplicationState : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                if(Application["Counter"] == null)
+                {
+                    Application["Counter"] = 0;
+                    lblCount.Text = "Counter: " + Application["Counter"].ToString();
+                }
+            }
+        }
+
+        protected void btnIncrement_Click(object sender, EventArgs e)
+        {
+            int counter = (int)Application["Counter"];
+            counter++;
+            Application["Counter"] = counter;
+            lblCount.Text = "Counter: " + counter.ToString();
+        }
+    }
+}
